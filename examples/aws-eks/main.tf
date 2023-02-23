819815f757b98b154c58eca2b1b6bdb9165cdc37
Aseer KT <aseer@wednesday.is>
2023-02-23 16:16:20 +0530

    chore: fix variable missing issue

--- examples/aws-eks/main.tf
+++ examples/aws-eks/main.tf
@@ -1,6 +1,9 @@
 module "aws_eks" {
   source = "../../modules/aws-eks"
 
+  aws_access_key_id     = var.aws_access_key_id
+  aws_access_secret_key = var.aws_access_secret_key
+
   application_name = "ws-test"
   region           = "ap-south-1"
 }
---
+++ examples/aws-eks/variables.tf
@@ -0,0 +1,11 @@
+variable "aws_access_key_id" {
+  type        = string
+  description = "AWS Access Key ID"
+  sensitive   = true
+}
+
+variable "aws_access_secret_key" {
+  type        = string
+  description = "AWS Access Secret Key"
+  sensitive   = true
+}
