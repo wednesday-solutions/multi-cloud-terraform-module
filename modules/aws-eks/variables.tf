f40f0d43e9bfa5a3815a10fcbc228da9aeaa0fe7
Aseer KT <aseer@wednesday.is>
2023-03-14 16:19:45 +0530

    refactor: move provider block outside module

--- modules/aws-eks/variables.tf
+++ modules/aws-eks/variables.tf
@@ -1,15 +1,3 @@
-variable "aws_access_key_id" {
-  type        = string
-  description = "AWS Access Key ID"
-  sensitive   = true
-}
-
-variable "aws_access_secret_key" {
-  type        = string
-  description = "AWS Access Secret Key"
-  sensitive   = true
-}
-
 variable "application_name" {
   type        = string
   description = "Name of application"
