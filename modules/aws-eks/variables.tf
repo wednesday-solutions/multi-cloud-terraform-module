b2f8cc311c3ac78fbc230e95386895ca4484ea9c
Aseer KT <aseer@wednesday.is>
2023-03-15 20:19:53 +0530

    feat: move to terraform for IaC of eks network config

--- modules/aws-eks/variables.tf
+++ modules/aws-eks/variables.tf
@@ -14,3 +14,27 @@ variable "enable_aws_load_balancer_controller_addon" {
   default     = true
   description = "Enable AWS load balancer controller addon for current cluster"
 }
+
+variable "vpc_cidr_block" {
+  type        = string
+  description = "VPC CIDR IP address block"
+}
+
+
+variable "public_subnets" {
+  description = "Public subnet configuration"
+  type = list(object({
+    name              = string
+    availability_zone = string
+    cidr_block        = string
+  }))
+}
+
+variable "private_subnets" {
+  description = "Private subnet configuration"
+  type = list(object({
+    name              = string
+    availability_zone = string
+    cidr_block        = string
+  }))
+}
