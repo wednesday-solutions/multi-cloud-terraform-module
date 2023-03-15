70ff62ac64b7075073955e580cc3820bfcf19379
Aseer KT <aseer@wednesday.is>
2023-03-15 20:37:24 +0530

    feat: gke auto module

---
+++ examples/gcp-gke/main.tf
@@ -0,0 +1,18 @@
+locals {
+  region = "asia-southeast1"
+}
+
+provider "google" {
+  project = var.project_id
+  region  = local.region
+}
+
+module "gcp_gke" {
+  source = "../../modules/gcp-gke"
+
+  project_id       = var.project_id
+  application_name = "gke-test-cluster"
+  region           = local.region
+
+  subnet_cidr_range = "10.0.0.0/16"
+}
